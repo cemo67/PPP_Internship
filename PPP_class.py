ca4eddc698d4393c4bad6c7b111e3b415ca311a9
cemo67 <tzcemo@gmail.com>
2020-11-24 12:27:45 +0100

    Cleaned Version of PPP_class

--- PPP_class.py
+++ PPP_class.py
@@ -25,32 +25,6 @@ class PPP_class:
                              np.arange(0, 101, bins_per_class_output),
                              axis=0).flatten()
 
-    def fit_origin(self, X_train_perturbed, y_train, Pertubations_List):
-        self.meta_features = []
-        self.meta_scores = []
-        predictions = self.classifier.predict_proba(X_train_perturbed)
-        self.meta_features.append(self.compute_ppp_features(predictions))
-        self.meta_scores.append(self.classifier.score(X_train_perturbed, y_train))
-
-
-        print('META', self.meta_features)
-        print(len(self.meta_features[0]))
-        #for index, element in enumerate(self.meta_features[0]):
-        #    print(index, element)
-
-
-        print('Proba', predictions)
-        print(len(predictions))
-#        plt.plot(self.meta_features)
-#        plt.show()
-
-        print('PPP Score', self.meta_scores)
-        print()
-        print('####################')
-        print()
-
-        self.predictor.fit(self.meta_features, self.meta_scores)
-
     def fit_ppp(self, y_train, Pertubations_List, file_information=None):
         self.meta_features = []
         self.meta_scores = []
@@ -70,68 +44,10 @@ class PPP_class:
 
         self.predictor.fit(self.meta_features, self.meta_scores)
 
-    def predict_ppp_origin(self, X_perturbed, hyperparameter=None):
+
+    def predict_ppp(self, X_perturbed):
         self.meta_features = []
         predictions = self.classifier.predict_proba(X_perturbed)
         self.meta_features.append(self.compute_ppp_features(predictions))
 
-        if hyperparameter != None:
-            self.meta_features = [np.insert(self.meta_features, 0, hyperparameter)]
-
         return self.predictor.predict(self.meta_features, return_std=True)
-
-    def predict_ppp(self, Pertubations_List, file_information= None):
-
-        for name, pertubation in Pertubations_List:
-            self.meta_features = []
-
-            X_test_perturbed = pertubation.perturbe()
-
-            predictions = self.classifier.predict_proba(X_test_perturbed)
-            self.meta_features.append(self.compute_ppp_features(predictions))
-            mu, sigma = self.predictor.predict(self.meta_features, return_std=True)
-
-            if file_information != None:
-                file_information.write(str(name) + ';' + str(mu) + ';' + str(sigma) + '\n')
-
-    def predict_all_models(self, y_test, Pertubations_List, models_list, file_information):
-        for name, pertubation in Pertubations_List:
-            print(Pertubations_List)
-            X_test_perturbed = pertubation.perturbe()
-            best_index = 0
-            best_mu = 0
-            best_sigma = 0
-
-            file_information.write(str(name) + ';')
-
-            for index, model in models_list:
-                print(index)
-                print(model)
-                self.meta_features = []
-                predictions = model.predict_proba(X_test_perturbed)
-                self.meta_features.append(self.compute_ppp_features(predictions))
-                mu, sigma = self.predictor.predict(self.meta_features, return_std=True)
-
-                file_information.write(str(mu) + ';' + str(sigma) + ';')
-
-                if mu > best_mu:
-                    best_index = index
-                    best_mu = mu
-                    best_sigma = sigma
-
-            file_information.write( str(best_index) + '\n')
-            file_information.write('Real Score;')
-
-            best_index = 0
-            best_mu = 0
-
-            for index, model in models_list:
-                real_score = model.score(X_test_perturbed, y_test)
-
-                if real_score > best_mu:
-                    best_index = index
-                    best_mu = real_score
-
-                file_information.write(str(real_score) + ';;' )
-
-            file_information.write( str(best_index) + '\n')
