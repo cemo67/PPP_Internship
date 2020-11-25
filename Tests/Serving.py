2af2a6fd7ef91459e2222ac5aeeccd0e74cb8b61
cemo67 <tzcemo@gmail.com>
2020-11-25 12:42:34 +0100

    Test Runs

--- Tests/Serving.py
+++ Tests/Serving.py
@@ -1,53 +1,73 @@
-import sys
-sys.path.append("../")
 import os
 import pickle
 from Data.toy_data_class import toy_data_class
 from PPP_class import PPP_class
-from Pertubations.Pertubation_List import get_pertubation_list, get_file_list
+from Pertubations.Pertubation_List import get_pertubation_list, get_config
 
 # Config
-SAVE = True
-DATA_NAME_LIST = get_file_list()
-MODEL_PATH = '../models/'
-models = []
+config_dict = get_config()
+
+DATA_NAME_LIST = config_dict['file_list']
+k_range = config_dict['k_range']
+samples_ = config_dict['samples']
 
 for Data_name in DATA_NAME_LIST:
     print(Data_name)
     print()
 
-    data = toy_data_class()
-    X_train, X_test, y_train, y_test = data.load(Data_name)
+    data = toy_data_class(samples=samples_, name=Data_name)
+    X_train, X_test, y_train, y_test = data.load()
 
     PERTUBATION_LIST = get_pertubation_list(X_test, data)
 
     MODEL_PATH = '../models/' + str(Data_name) + '/'
 
-    for folder in os.listdir(MODEL_PATH):
-        MODEL_PATH_TEMP = os.path.join(MODEL_PATH, folder)
+    file_information = open(MODEL_PATH + '/PPP_test_All.csv', 'w')
 
-        # If Model
-        if os.path.isdir(MODEL_PATH_TEMP):
-            print(MODEL_PATH_TEMP)
-            classifier = (pickle.load(open(MODEL_PATH_TEMP + '/classifier.pickle', 'rb')))
-            predictor = pickle.load(open(MODEL_PATH_TEMP + '/predictor.pickle', 'rb'))
-            models.append([str(folder), classifier])
+    file_information.write('Pertubation;')
+    for i in range(k_range):
+        file_information.write('Model;Mu;Sigma;Real Score;')
+    file_information.write('BEST GP;BEST REAL\n')
 
-    if SAVE:
-        file_information = open(MODEL_PATH + '/PPP_test_All.csv', 'w')
-        file_information.write('Pertubation')
-        last_lement = models[-1][0]
+    for name, pertubation in PERTUBATION_LIST:
 
-        for index, element in models:
-            file_information.write(';' + str(index) + ' Mu ; Sigma')
+        X_test_perturbed = pertubation.perturbe()
+        best_index_GP = ''
+        best_mu_GP = 0
+        best_index_REAL = ''
+        best_mu_REAL = 0
+        file_information.write(str(name) + ';')
 
-        file_information.write(';Best model\n')
+        for folder in os.listdir(MODEL_PATH):
+            MODEL_PATH_TEMP = os.path.join(MODEL_PATH, folder)
+            index = folder.split('/')[-1]
 
-    ppp = PPP_class(classifier, predictor)
+            if os.path.isdir(MODEL_PATH_TEMP):
+                print(MODEL_PATH_TEMP)
+                classifier = (pickle.load(open(MODEL_PATH_TEMP + '/classifier.pickle', 'rb')))
+                predictor = pickle.load(open(MODEL_PATH_TEMP + '/predictor.pickle', 'rb'))
+            else:
+                continue
 
-    ppp.predict_all_models(y_test, PERTUBATION_LIST, models, file_information)
+            ppp = PPP_class(classifier=classifier, predictor=predictor)
+            mu, sigma = ppp.predict_ppp(X_test_perturbed)
+            real_score = classifier.score(X_test_perturbed, y_test)
+            
+            if mu > best_mu_GP:
+                best_mu_GP = mu
+                best_index_GP = str(index)
+            elif mu == best_mu_GP:
+                best_index_GP += ' -- ' + str(index)
+                
+            if real_score > best_mu_REAL:
+                best_mu_REAL = real_score
+                best_index_REAL = str(index)
+            elif real_score == best_mu_REAL:
+                best_index_REAL += ' -- ' + str(index)
 
-    if SAVE:
-        file_information.close()
+            file_information.write( str(index) + ';' + str(mu) + ';' + str(sigma) + ';' + str(real_score) + ';')
 
-print('END!')
+        file_information.write(str(best_index_GP) + ';' + str(best_index_REAL) + '\n')
+
+    file_information.close()
+print('DONE!')
--- Tests/Train.py
+++ Tests/Train.py
@@ -1,24 +1,23 @@
-import sys
-sys.path.append("../")
 from Data.toy_data_class import toy_data_class
-from Pertubations.Pertubation_List import get_pertubation_list, get_file_list
+from Pertubations.Pertubation_List import get_pertubation_list, get_config
 from PPP_class import PPP_class
 from sklearn.neighbors import KNeighborsClassifier
 import os
 import pickle
 
 # Config
-SAVE = True
-DATA_NAME_LIST = get_file_list()
-k_range = 15
-best_sore = [1, 0] # [HP , Score]
+config_dict = get_config()
+
+DATA_NAME_LIST = config_dict['file_list']
+k_range = config_dict['k_range']
+samples_ = config_dict['samples']
 
 for Data_name in DATA_NAME_LIST:
     print(Data_name)
     print()
 
-    data = toy_data_class()
-    X_train, X_test, y_train, y_test = data.load(Data_name)
+    data = toy_data_class(samples=samples_, name = Data_name)
+    X_train, X_test, y_train, y_test = data.load()
 
     PERTUBATION_LIST = get_pertubation_list(X_train, data)
 
@@ -31,40 +30,26 @@ for Data_name in DATA_NAME_LIST:
         # Classifier
         classifier = KNeighborsClassifier(n_neighbors=hp_1)
         classifier.fit(X_train, y_train)
-        score = classifier.score(X_train, y_train)
-        if score > best_sore[1]:
-            best_sore[0] = hp_1
-            best_sore[1] = score
 
         # Predictor
         ppp = PPP_class(classifier=classifier)
 
-        if SAVE:
-            # Save Folder
-            MODEL_PATH_TEMP = MODEL_PATH + str(hp_1) + '/'
-            os.makedirs(MODEL_PATH_TEMP)
+        # Save Folder
+        MODEL_PATH_TEMP = MODEL_PATH + str(hp_1) + '/'
+        os.makedirs(MODEL_PATH_TEMP)
 
-            # Write CSV
-            file_information = open(MODEL_PATH_TEMP + 'PPP_train.csv', 'w')
-            file_information.write('Pertubation;Output_Score\n')
+        # Write CSV
+        file_information = open(MODEL_PATH_TEMP + 'PPP_train.csv', 'w')
+        file_information.write('Pertubation;Output_Score\n')
 
-            ppp.fit_ppp(y_train, PERTUBATION_LIST, file_information)
-        else:
-            ppp.fit_ppp(y_train, PERTUBATION_LIST)
+        ppp.fit_ppp(y_train, PERTUBATION_LIST, file_information)
 
-        if SAVE:
-            # Classifier
-            pickle.dump(ppp.classifier, open(MODEL_PATH_TEMP + 'classifier.pickle', 'wb'))
+        # Classifier
+        pickle.dump(ppp.classifier, open(MODEL_PATH_TEMP + 'classifier.pickle', 'wb'))
 
-            # Predictor
-            pickle.dump(ppp.predictor, open(MODEL_PATH_TEMP + 'predictor.pickle', 'wb'))
+        # Predictor
+        pickle.dump(ppp.predictor, open(MODEL_PATH_TEMP + 'predictor.pickle', 'wb'))
 
-            file_information.close()
-
-    if SAVE:
-        file_best_model = open(MODEL_PATH + 'best_model.txt', 'w')
-        file_best_model.write('HP, Best Score\n')
-        file_best_model.write(str(best_sore))
-        file_best_model.close()
+        file_information.close()
 
 print('DONE!')
