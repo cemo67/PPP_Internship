d1a4128f9d174a2f704b1bc99e21c50f93e8eaef
cemo67 <tzcemo@gmail.com>
2020-11-30 14:11:24 +0100

    ADD Data Generator

---
+++ Data/PPP_generate_toy_data.py
@@ -0,0 +1,43 @@
+from Pertubations.Pertubation_List import get_config
+from Data.toy_data_class import toy_data_class
+
+
+config_dict = get_config()
+DATA_NAME_LIST = config_dict['file_list']
+samples_ = config_dict['samples']
+
+bl = False
+mo = False
+ci = False
+co = False
+
+for name in DATA_NAME_LIST:
+    for sample in samples_:
+        t = toy_data_class(samples=sample, name=name)
+
+        if name == 'blobs':
+            t.blobs()
+            if bl:
+                print(name)
+                bl = False
+                t.plot_data()
+        elif name == 'moons':
+            t.moons()
+            if mo:
+                print(name)
+                mo = False
+                t.plot_data()
+        elif name == 'circles':
+            t.circles()
+            if ci:
+                print(name)
+                ci = False
+                t.plot_data()
+        elif name == 'cosine':
+            t.cosine()
+            if co:
+                print(name)
+                co = False
+                t.plot_data()
+
+        t.save()
